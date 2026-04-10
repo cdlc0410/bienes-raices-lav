c3024544961a273dddb4f9f01d49a90412b5cd9d
Carlos Lavieri <carloslaviericoronado@gmail.com>
2026-04-10 17:38:24 -0400

    feat: se agrega el darkmode ajustado a la preferencia de sistema

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,25 @@ function navResponsive() {
 }
 
 function darkMode() {
+
+    const preferenceMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(preferenceMode.matches);
+
+    if (preferenceMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    preferenceMode.addEventListener('change', () => {
+        if (preferenceMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const btnDarkMode = document.querySelector('.dark-mode-boton');
 
     btnDarkMode.addEventListener('click', () => {
